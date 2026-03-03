98ca63160deb88187538e3131db263d54a6727d6
Sergej Gavrilov <sgavrilov@kopfdreher.de>
2026-03-03 16:35:20 +0100

    added floor/ceiling colors

--- src/key_press_handler.c
+++ src/key_press_handler.c
@@ -6,7 +6,7 @@
 /*   By: sgavrilo <sgavrilo@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 15:29:42 by sgavrilo          #+#    #+#             */
-/*   Updated: 2026/03/03 15:37:24 by sgavrilo         ###   ########.fr       */
+/*   Updated: 2026/03/03 16:11:43 by sgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,13 @@ int	key_press_handler(int keycode, t_game *game)
 {
 	if (keycode == K_ESC)
 		close_window_handler(game);
+	/*
+	 * if (keycode == K_W)
+	 * if (keycode == K_A)
+	 * if (keycode == K_S)
+	 * if (keycode == K_D)
+	 * if (keycode == K_LEFT)
+	 * if (keycode == K_RIGHT)
+		*/
 	return (SUCCESS);
 }
--- src/render_next_frame.c
+++ src/render_next_frame.c
@@ -6,14 +6,39 @@
 /*   By: sgavrilo <sgavrilo@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 15:02:34 by sgavrilo          #+#    #+#             */
-/*   Updated: 2026/03/03 15:15:44 by sgavrilo         ###   ########.fr       */
+/*   Updated: 2026/03/03 16:34:22 by sgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+static void	put_ceiling_floor(t_game *game)
+{
+	int	x;
+	int	y;
+
+	// remove dummy colors
+	game->config.floor_color = 0xff160c28;
+	game->config.ceiling_color = 0xffefcb68;
+	x = -1;
+	while (++x < SCREEN_WIDTH)
+	{
+		y = -1;
+		while (++y < SCREEN_HEIGHT)
+		{
+			if (y < SCREEN_HEIGHT / 2)
+				my_mlx_pixel_put(&game->gfx.buff,
+					x, y, game->config.ceiling_color);
+			else
+				my_mlx_pixel_put(&game->gfx.buff,
+					x, y, game->config.floor_color);
+		}
+	}
+}
+
 int		render_next_frame(t_game *game)
 {
+	put_ceiling_floor(game);
 	mlx_put_image_to_window(game->gfx.mlx, game->gfx.win, game->gfx.buff.img, 0, 0);
 	return (SUCCESS);
 }
